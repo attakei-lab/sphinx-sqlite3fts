5947a13b9424b61e02d699409cd846acb9e065b5
Kazuya Takei <myself@attakei.net>
2022-11-24 10:33:08 +0900

    refactor: module-level importing

--- src/sphinx_sqlite3fts/__init__.py
+++ src/sphinx_sqlite3fts/__init__.py
@@ -1,17 +1,15 @@
 """Sphinx document searcher using SQLite3."""
 from sphinx.application import Sphinx
 
-from . import events
+from . import builders, events
 
 __version__ = "0.0.2"
 
 
 def setup(app: Sphinx):
     """Entrypoint as Sphinx extension."""
-    from .builders import SqliteBuilder
-
     app.add_config_value("sqlite3fts_exclude_pages", [], "env")
-    app.add_builder(SqliteBuilder)
+    app.add_builder(builders.SqliteBuilder)
     app.connect("builder-inited", events.configure_database)
     app.connect("html-page-context", events.register_document)
     app.connect("build-finished", events.save_database)
